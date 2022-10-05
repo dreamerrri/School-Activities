28736f7ac8aeb1784f253439c8238ecba1000943
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 13:22:29 +0800

    Create Numbers Yes or No.py

---
+++ Numbers Yes or No.py
@@ -0,0 +1,7 @@
+# A python program that will continuously ask for number input from the user.
+# The program will only stop when the user input a value of zero.
+# Could use some work and optimization.
+count = 0
+num = int(input("Enter a number: "))
+while count < num:
+    num = int(input("Enter a number: "))
