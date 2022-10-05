9b3427f862fcf741ec889d2917d02faaf3fd2258
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 11:24:18 +0800

    Create Odd or Even identifier.py

---
+++ Odd or Even identifier.py
@@ -0,0 +1,11 @@
+#Python program that will ask a number input from the user, then determine whether the inputted number is a ODD number or EVEN.
+
+input = int(input("Enter a number: "))
+
+X = input % 2
+
+print(" ")
+if X == 0:
+  print("EVEN")
+else:
+  print("ODD")
