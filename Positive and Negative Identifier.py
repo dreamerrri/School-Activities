f4858c403dc144b7ec0d2e2a8a73f44ab6a4d9c1
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 11:12:25 +0800

    Create Positive and Negative Identifier.py

---
+++ Positive and Negative Identifier.py
@@ -0,0 +1,9 @@
+#Python program that will ask a number input from the user, then determine whether the inputted number is a POSITIVE number or NEGATIVE.
+
+input = int(input("Enter a Number: "))
+
+print(" ")
+if input < 1:
+    print("Negative")
+else:
+    print("Positive")
