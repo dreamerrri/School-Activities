5f0dd43ed418f9b21f4cb29d60079d3a05d01609
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 15:30:09 +0800

    Create Token Dispenser Simulator.py

---
+++ Token Dispenser Simulator.py
@@ -0,0 +1,13 @@
+# A program that emulates a Token Dispenser.
+
+token = int(input("Enter token stocks: "))
+dispense = int(input("How many do you wish to dispense?: "))
+
+print(" ")
+if token >= dispense:
+    for i in range(0, dispense):
+        print("token")
+elif token < dispense:
+    for i in range(0, token):
+        print("token")
+    print("Exceeded amount of Tokens")
