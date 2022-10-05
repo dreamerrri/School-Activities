e14e5df0db0d57933bdf1143a19a9aeeae90e33b
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 14:36:39 +0800

    Create Sum of an Input.py

---
+++ Sum of an Input.py
@@ -0,0 +1,9 @@
+# Simple program that adds every digit from an inputted variable.
+
+num = int(input("Enter a Number: "))
+sum = 0
+
+for num in range(sum, num+1):
+    sum += num
+print(" ")
+print("The Sum is: ", sum)
