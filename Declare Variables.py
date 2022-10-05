00de6bf96cac7c10feec6ac3477c80ef26dc6a03
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-05 11:46:50 +0800

    Update Declare Variables.py

--- Declare Variables.py
+++ Declare Variables.py
@@ -1,4 +1,5 @@
-#Write a python program that declare 2 variables (num1 and num2) with the values 105 and 35 respectively.
+#Python program that declares 2 variables (num1 and num2) with the values 105 and 35 respectively.
+#Numbers 1 & 2 are interchangeable with any number.
 
 Num1 = 105
 Num2 = 35
