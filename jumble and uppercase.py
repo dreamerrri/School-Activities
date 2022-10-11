c60fe0bc371dd21346221f96f30fb20a009cc227
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-11 13:52:49 +0800

    Create jumble and uppercase.py

---
+++ jumble and uppercase.py
@@ -0,0 +1,16 @@
+def info(a, b, c):
+    a = a.upper()
+    print(" ")
+    print("Name: ", a)
+    b = b.upper()
+    print("Course: ", b)
+    c = c.upper()
+    print("School: ", c)
+
+
+x = input("Enter your Name: ")
+y = input("Enter your Course: ")
+z = input("Enter your School: ")
+
+
+info(x, y, z)
