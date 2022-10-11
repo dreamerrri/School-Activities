1c9dac6817b28d51997753488a722611a5c41880
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-11 14:54:21 +0800

    Update Human years to Dog years.py

--- Human years to Dog years.py
+++ Human years to Dog years.py
@@ -1,5 +1,8 @@
 # A python program that will compute for the human age(1-10 years old) equivalent to the age of dogs.
 # I still do not know how to seperate outputs like a table, so pardon the heinous spaces, it would help if somebody teaches me how.
+#   print("Human Age\t\tDog Age")  - try this sometime
+
+
 
 h = int(10)
 i = 0
